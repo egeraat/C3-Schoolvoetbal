a8ca6a9191581561fc65b9d87846335323a7ded7
Bramd3 <304625@edu.curio.nl>
2024-12-02 09:54:58 +0100

    css login en register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,31 +1,31 @@
-<form method="POST" action="{{ route('register') }}">
-    <link rel="stylesheet" href="{{ asset('app.css') }}">
-    @csrf
+<div class="auth-container register-container">
+    <form method="POST" action="{{ route('register') }}">
+        <link rel="stylesheet" href="{{ asset('app.css') }}">
+        @csrf
 
-    <h2>Registreren</h2>
+        <h2>Registreren</h2>
 
-   
+        <label for="name">Naam</label>
+        <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
+        @error('name')
+            <div class="error">{{ $message }}</div>
+        @enderror
 
-    <label for="name">Naam</label>
-    <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
-    @error('name')
-        <div class="text-red-500">{{ $message }}</div>
-    @enderror
+        <label for="email">E-mailadres</label>
+        <input id="email" type="email" name="email" value="{{ old('email') }}" required>
+        @error('email')
+            <div class="error">{{ $message }}</div>
+        @enderror
 
-    <label for="email">E-mailadres</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required>
-    @error('email')
-        <div class="text-red-500">{{ $message }}</div>
-    @enderror
+        <label for="password">Wachtwoord</label>
+        <input id="password" type="password" name="password" required>
+        @error('password')
+            <div class="error">{{ $message }}</div>
+        @enderror
 
-    <label for="password">Wachtwoord</label>
-    <input id="password" type="password" name="password" required>
-    @error('password')
-        <div class="text-red-500">{{ $message }}</div>
-    @enderror
+        <label for="password_confirmation">Wachtwoord bevestigen</label>
+        <input id="password_confirmation" type="password" name="password_confirmation" required>
 
-    <label for="password_confirmation">Wachtwoord check</label>
-    <input id="password_confirmation" type="password" name="password_confirmation" required>
-
-    <button type="submit">Registreren</button>
-</form>
+        <button type="submit">Registreren</button>
+    </form>
+</div>
